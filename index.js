1a87ad135984d10b97b9b0f88be5a2a74f37be8c
VincentLeal <leal93190@gmail.com>
2018-04-10 17:50:09 +0200

    code clear

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 const Koa = require('koa');
-//const Router = require('koa-router');
 const BodyParser = require('koa-bodyparser');
 const router = require('./routes');
 
 const app = new Koa();
-//const router = new Router();
-
 app.use(BodyParser());
-
-/*router.get("/", async function (ctx){
-  ctx.body = {message: "Ta race"}
-});
-
-router.post("/", async function (ctx){
-  let name = ctx.request.body.name || "World";
-  ctx.body = {message: `Hello ${name}!`}
-});
-*/
-
 app
     .use(router.allowedMethods())
     .use(router.routes());
@@ -26,5 +12,3 @@ app.listen(3000);
 console.log("Running on port 3000");
 
 module.exports = app;
-
-//app.listen(3000);
